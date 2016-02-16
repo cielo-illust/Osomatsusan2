495c90e25b9c8fbaaa06331dbc135c18518ba12d
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2016-02-16 20:38:21 +0900

    #02

---
+++ Osomatsusan2/Assets/scripts/charactorController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charactorController : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("left")) {
+			transform.Rotate(new Vector3(0f,-1.0f,0f)); 
+		}
+		if (Input.GetKey ("right")) {
+			transform.Rotate(new Vector3(0f,1.0f,0f)); 
+		}
+		if (Input.GetKeyDown ("up")) {
+			animator.SetBool ("walk", true);
+		}
+		if (Input.GetKeyUp ("up")) {
+			animator.SetBool ("walk", false);
+		}
+	}
+}
